730e4b34f7235658730363a303b3e35d93633fb7
peijun <contact.zouri.music@gmail.com>
2020-08-08 21:01:12 +0900

    test: GoogleCalenderのコマンドパースのテスト

---
+++ tests/test_calendar.py
@@ -0,0 +1,10 @@
+from logic.calendar import CalendarImpl
+import json
+
+sample_command = "testtitle -d 10/10 -l japan"
+
+def test_command_parse():
+    result = CalendarImpl.command_parse(text=sample_command)
+    obj = {"value1" : "10/10", "value2" : "testtitle", "value3" : "japan"} 
+    json_data = json.dumps(obj).encode("utf-8")
+    assert result == json_data
